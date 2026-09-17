a15a997e46041ba192b6bac810cfa6194f559eb9
Martin Tomassi <martin.tomassi@studio.unibo.it>
2026-09-17 19:27:46 +0200

    refactor: modified project package structure for repositories and entities

--- server/worker/src/main/java/it/unibo/hermes/worker/entity/cassandra/MessageById.java
+++ server/worker/src/main/java/it/unibo/hermes/worker/entity/cassandra/MessageById.java
@@ -1,4 +1,4 @@
-package it.unibo.hermes.worker.entity;
+package it.unibo.hermes.worker.entity.cassandra;
 
 import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
